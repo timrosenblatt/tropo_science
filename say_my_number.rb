19e76a20632ae27efbdf1e8d4688fe64803b7dfc
Tim Rosenblatt <github@timrosenblatt.com>
2009-03-19 23:17:28 -0400

    the phone number gets split up, so that it doesn't pronounce as billions, millions, etc. also added spacing

--- say_my_number.rb
+++ say_my_number.rb
@@ -1,5 +1,14 @@
 answer
 
-say "S-up. Your phone number is #{$currentCall.callerID}"
+digits = $currentCall.callerID.to_s.split('')
+
+area_code = digits[0..2]
+city_code = digits[3..5]
+subscriber_number = digits[6..9]
+
+
+# single dashes get spoken as 'dash', use doubles for a pause.
+# Double commas don't work, neither do extra spaces
+say "-- -- -- S-up. Your phone number is -- #{area_code.join(',')}--#{city_code.join(',')}--#{subscriber_number.join(',')}"
 
 hangup
